62609243280d8d9c2322a969ceeb3c91858fa3c7
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-04-13 14:15:18 +0100

    stop double redirect

--- server/controllers/saveEmail.js
+++ server/controllers/saveEmail.js
@@ -3,31 +3,31 @@ const shared = require('../shared')
 exports.saveEmail = (req, res) => {
   if(req.body.checkbox) { // Avoid bots
     res.redirect('/')
-  }
-
-  req.checkBody('email', 'error').isEmail()
-  const errors = req.validationErrors()
-
-  if(errors) {
-    const dataPage = {
-      ...shared,
-      result: {
-        class: 'result error',
-        msg: 'Please insert a valid email'
-      }
-    }
-    res.status(400).render('form', dataPage)
   } else {
-    const dataPage = {
-      ...shared,
-      result: {
-        class: 'result success',
-        msg: 'Welcome to TrekBase'
-      },
-      subtitle: '*Thanks for sign up*',
-      formClass: 'hide'
+    req.checkBody('email', 'error').isEmail()
+    const errors = req.validationErrors()
+
+    if(errors) {
+      const dataPage = {
+        ...shared,
+        result: {
+          class: 'result error',
+          msg: 'Please insert a valid email'
+        }
+      }
+      res.status(400).render('form', dataPage)
+    } else {
+      const dataPage = {
+        ...shared,
+        result: {
+          class: 'result success',
+          msg: 'Welcome to TrekBase'
+        },
+        subtitle: '*Thanks for sign up*',
+        formClass: 'hide'
+      }
+      res.status(201).render('form', dataPage)
     }
-    res.status(201).render('form', dataPage)
   }
 }
 
